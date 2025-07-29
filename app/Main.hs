6cb03fbc3deedf297f0f2acd67a0f6b06235dd25
nicolemaracaja <nicole.brito.maracaja@ccc.ufcg.edu.br>
2025-07-29 20:51:28 -0300

    exibindo nome na main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import Tabuleiro
+import Arte
 
 main :: IO ()
 main = do
+    exibirInicio
     let tabGrande = criarTabuleiroGrande
     putStrLn "=== Tabuleiro Pequeno Cru ==="
     putStrLn $ mostrarTabuleiroPequenoCrua (head (head tabGrande))
